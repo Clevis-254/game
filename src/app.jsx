c6a3ba65033ea47bcb90c54fe60359236bf4416d
Jack Thomas <thomasj109@cardiff.ac.uk>
2024-11-30 16:47:58 +0000

    Nav bar with css

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,46 @@
-import { useState } from 'react';
+import React from "react";
 
-const App = ({ data }) => {
+const App = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          [Game Name Here]
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample07"
+          aria-controls="navbarsExample07"
+          aria-expanded="true"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    const [count, setCount] = useState(0);
-
-    return (
-        <main>
-            <h1>App</h1>
-            <p>Lorem Ipsum</p>
-            <div>
-                <div>{count}</div>
-                <button onClick={() => setCount(count + 1)}>Count</button>
-            </div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </main>
-    );
+        <div className="navbar-collapse collapse show" id="navbarsExample07">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Play <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My Stats
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                User Stats
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default App
+export default App;
