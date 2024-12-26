df31de90a8d9b6e17c51010a5bd2d3c788d175f8
c22065407 <thomasj109@cardiff.ac.uk>
2024-12-26 20:19:35 +0000

    Layout and styling complete.

--- src/MyStats.jsx
+++ src/MyStats.jsx
@@ -10,18 +10,19 @@ export function MyStats(){
             <br/>
 
             <div className="container">
-
-                <div className="statSection">
+                <div className="statSection1">
                     <h2 className="text-uppercase">Time Played</h2>
                     <h4>00:00:00</h4>
                 </div>
 
-                <div className="statSection">
+                <div className="statSection1">
                     <h2 className="text-uppercase">Audio Files Played</h2>
                     <h4>0</h4>
                 </div>
+            </div>
 
-                <div className="statSection">
+            <div className="container">
+                <div className="statSection2">
                     <h2 className="text-uppercase">Audio File Play Count</h2>
                     <h4>hit: 0</h4>
                     <h4>miss: 0</h4>
@@ -29,7 +30,7 @@ export function MyStats(){
                     <h4>stamina: 0</h4>
                 </div>
 
-                <div className="statSection">
+                <div className="statSection2">
                     <h2 className="text-uppercase">Command Usage</h2>
                     <h4>Repeat: 0</h4>
                     <h4>End Game: 0</h4>
@@ -37,10 +38,11 @@ export function MyStats(){
                     <h4>Go Slower: 0</h4>
                     <h4>Restart: 0</h4>
                 </div>
+            </div>
 
-
+            <div className="container">
                 <div className="statSectionHeatmap">
-                    <h2 className="text-uppercase">Heatmap</h2>
+                    <h1 className="text-uppercase">Heatmap</h1>
                     <img
                         src="https://talos-interactive.com/wp-content/uploads/2022/04/GameTelemetryGallery05-1024x576.jpg"/>
                 </div>
