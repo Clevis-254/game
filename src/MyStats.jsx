684ed17d6056c9782faf823f6cd4ca09360c3c85
c22065407 <thomasj109@cardiff.ac.uk>
2024-12-26 19:26:01 +0000

    Flex box layout implemented.

--- src/MyStats.jsx
+++ src/MyStats.jsx
@@ -4,39 +4,47 @@ import { Banner } from "./src/Banner";
 export function MyStats(){
     return(
         <>
-            <Banner />
+            <Banner/>
             <h1 style={{textAlign: "center"}}>My Stats</h1>
-            <hr /><br />
+            <hr/>
+            <br/>
 
-            <div className="text-center">
-                <h2>Time Played</h2>
-                <h3>00:00:00</h3>
-                <br />
-                <h2>Audio Files Played</h2>
-                <h3>0</h3>
-                <br />
-                <h2>Audio File Play Count</h2>
-                <h3>hit: 0</h3>
-                <h3>miss: 0</h3>
-                <h3>damaged: 0</h3>
-                <h3>stamina: 0</h3>
+            <div className="container">
+
+                <div className="statSection">
+                    <h2 className="text-uppercase">Time Played</h2>
+                    <h4>00:00:00</h4>
+                </div>
+
+                <div className="statSection">
+                    <h2 className="text-uppercase">Audio Files Played</h2>
+                    <h4>0</h4>
+                </div>
+
+                <div className="statSection">
+                    <h2 className="text-uppercase">Audio File Play Count</h2>
+                    <h4>hit: 0</h4>
+                    <h4>miss: 0</h4>
+                    <h4>damaged: 0</h4>
+                    <h4>stamina: 0</h4>
+                </div>
+
+                <div className="statSection">
+                    <h2 className="text-uppercase">Command Usage</h2>
+                    <h4>Repeat: 0</h4>
+                    <h4>End Game: 0</h4>
+                    <h4>Go Faster: 0</h4>
+                    <h4>Go Slower: 0</h4>
+                    <h4>Restart: 0</h4>
+                </div>
+
+
+                <div className="statSectionHeatmap">
+                    <h2 className="text-uppercase">Heatmap</h2>
+                    <img
+                        src="https://talos-interactive.com/wp-content/uploads/2022/04/GameTelemetryGallery05-1024x576.jpg"/>
+                </div>
             </div>
-
-            <br />
-
-            <div className="text-center">
-                <h2>Command Usage</h2>
-                <h3>Repeat: 0</h3>
-                <h3>End Game: 0</h3>
-                <h3>Go Faster: 0</h3>
-                <h3>Go Slower: 0</h3>
-                <h3>Restart: 0</h3>
-            </div>
-
-            <br /><hr />
-
-            <h2>Heatmap</h2>
-            <img src="https://talos-interactive.com/wp-content/uploads/2022/04/GameTelemetryGallery05-1024x576.jpg"/>
         </>
     )
 }
